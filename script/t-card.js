eb728853d24c2cd2232721c4773dcbdc4b126c8c
ling <ling95613@163.com>
2017-11-30 17:07:38 +0800

    PC和移动端适应

---
+++ script/t-card.js
@@ -0,0 +1,8 @@
+/**
+ *  created by ling on 2017-11-30 16:58.
+ */
+;(function () {
+    var winHeight  = window.innerHeight || document.documentElement.clientHeight;
+    // document.querySelector()
+    console.log(winHeight)
+})()
