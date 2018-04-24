3b141d299e18eb0baeedb5048f3236a5a2bcc9d4
lyin <andesome95@gmail.com>
2018-04-24 14:43:20 +0800

    增加切换按钮

--- script/t-card.js
+++ script/t-card.js
@@ -1,8 +1,8 @@
 /**
  *  created by ling on 2017-11-30 16:58.
  */
-;(function () {
-    var winHeight  = window.innerHeight || document.documentElement.clientHeight;
-    // document.querySelector()
-    console.log(winHeight)
-})()
+; (function () {
+  const winHeight = window.innerHeight || document.documentElement.clientHeight;
+  // document.querySelector()
+  console.log(winHeight);
+}());
